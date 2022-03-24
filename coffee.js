caf743f414220835ef1ef0ffd1e35fc60a5d92a3
minjirungE <minji2003032@ewhain.net>
2022-03-24 19:40:59 +0900

    input event keydown

---
+++ coffee.js
@@ -0,0 +1,11 @@
+
+button.addEventListener("input",
+    function(e) {
+        console.log(e.target.value)
+    }
+)
+
+
+function hi() {
+    alert("hi")
+}
---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded",
+    funtion(e) {
+        let button = document.querySelector("input")
+        button.addEventListener("input",
+            function(e) {
+                console.log(e.target.value)
+            }
+        )
+    }
+)
+
+function hi() {
+    alert("hi")
+}
